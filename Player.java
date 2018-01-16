c8105620cd4894c4e006c33b91d9eab4e323dd28
Edward Chen <edward98c@gmail.com>
2018-01-16 00:51:15 -0800

    Random movement player

--- Player.java
+++ Player.java
@@ -18,7 +18,7 @@ public class Player {
 
         // Direction is a normal java enum.
         Direction[] directions = Direction.values();
-        int rand = Math.random() * 8;
+        int rand = (int) Math.random() * 8;
 
         while (true) {
             System.out.println("Current round: "+gc.round());
@@ -28,17 +28,12 @@ public class Player {
                 Unit unit = units.get(i);
 
                 // Most methods on gc take unit IDs, instead of the unit objects themselves.
-               if (unit.id() == worker_id) {
-                    if (false) {
+               if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), direction[rand])) {
+                    gc.moveRobot(unit.id(), direction[rand]);
+                }
+                else {
 
-                    }
-                    else if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), direction[rand])) {
-                        gc.moveRobot(unit.id(), direction[rand]);
-                    }
-                    else {
-
-                    }
-            	}
+                }
             }
             // Submit the actions we've done, and wait for our next turn.
             gc.nextTurn();
